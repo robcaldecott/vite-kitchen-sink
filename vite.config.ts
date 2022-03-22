abd2501e86f28c80e9140029278d9d7e84ec54de
Rob Caldecott <robert.caldecott@keyloop.com>
2022-03-22 08:45:09 +0000

    Package updates. Added bundle visualiser.

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@
 
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,6 +15,7 @@ export default defineConfig({
         plugins: ["formatjs"],
       },
     }),
+    visualizer(),
   ],
   test: {
     global: true,
